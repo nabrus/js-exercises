7bbe88bbae04d77793df8f0a13219758408a861b
towelie <34988577+nabrus@users.noreply.github.com>
2018-12-21 22:26:27 -0500

    commented out unused code from webpack config

--- wesbos-es6/JS_modules_and_npm/es6modules/webpack.config.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/webpack.config.js
@@ -1,6 +1,6 @@
 // const webpack = require("webpack");
 
-const nodeEnv = process.env.NODE_ENV || "production";
+// const nodeEnv = process.env.NODE_ENV || "production";
 
 module.exports = {
   mode: "production",
