97c3d7e8ca3a285dac32247fedf2ee7010ca54b8
towelie <34988577+nabrus@users.noreply.github.com>
2018-12-24 20:39:37 -0500

    add module comments

--- wesbos-es6/JS_modules_and_npm/es6modules/app.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/app.js
@@ -1,8 +1,8 @@
 import { uniq } from "lodash";
 import insane from "insane";
 import jsonp from "jsonp";
-import apiKey from "./src/config";
-console.log(apiKey);
+import apiKey from "./src/config"; // when using a default export any
+console.log(apiKey);               // variable (apiKey) name can be used
 
 
 const ages = [1,14,56,5];
--- wesbos-es6/JS_modules_and_npm/es6modules/src/config.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/src/config.js
@@ -1,3 +1,3 @@
 const apiKey = "abc123";
-
+// default export is for the main thing the module does
 export default apiKey;
