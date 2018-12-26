615126dfcd03f7d8e29df8098f55a9be19870eb9
towelie <34988577+nabrus@users.noreply.github.com>
2018-12-25 23:26:39 -0500

    complete creating own modules

--- wesbos-es6/JS_modules_and_npm/es6modules/app.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/app.js
@@ -1,10 +1,18 @@
 import { uniq } from "lodash";
 import insane from "insane";
 import jsonp from "jsonp";
-import apiKey from "./src/config"; // when using a default export any
-console.log(apiKey);               // variable (apiKey) name can be used
+import {
+  apiKey as key,
+  url,
+  sayHi,
+  old,
+  cat,
+} from "./src/config";
 
+console.log(key, url, old, cat);
 
-const ages = [1,14,56,5];
+sayHi("Steve");
+
+const ages = [1, 14, 56, 5];
 
 console.log(uniq(ages));
--- wesbos-es6/JS_modules_and_npm/es6modules/src/config.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/src/config.js
@@ -1,13 +1,34 @@
-const apiKey = "abc123";
+// Named Export
+export const apiKey = "abc123";
+
+export const url = "http:/wesbos.com";
+
+export function sayHi(name) {
+  console.log(`Hello there ${name}`);
+}
+
+const age = 1000;
+const cat = "Mose";
+
+export { age as old, cat };
 /*
-  default export and named export
+  Default Export and Named Export
 
   default export: generally used to export the main
-  part of what the module does.
+  part of what the module does. When importing a default export
+  any variable name (apiKey) can be used.
 
-  named export: used to pull individual methods, varialbes, etc
-  from the module.
+  named export: Named exports are useful to export several values.
+  During the import, it is mandatory to use the same name of the
+  corresponding object.
+
+  Note: Modules can have only one default export, but can have
+  multiple named exports.
 */
 
-// exporting as the default
-export default apiKey;
+/*
+  exporting as the default. Note: default exports get renamed
+  to whatever name given when imported.
+*/
+
+// export default apiKey;
