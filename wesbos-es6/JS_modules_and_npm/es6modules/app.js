215c6361c7733c56a338dabb06b4378da0a07ed6
towelie <34988577+nabrus@users.noreply.github.com>
2018-12-27 14:56:47 -0500

    add more comments to created modules

--- wesbos-es6/JS_modules_and_npm/es6modules/app.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/app.js
@@ -1,13 +1,15 @@
 import { uniq } from "lodash";
 import insane from "insane";
 import jsonp from "jsonp";
+// import apiKey from "./src/config"; // import default syntax
 import {
   apiKey as key,
   url,
   sayHi,
   old,
   cat,
-} from "./src/config";
+} from "./src/config"; // import named variable syntax
+// `as` keyword used to rename variable on import
 
 console.log(key, url, old, cat);
 
--- wesbos-es6/JS_modules_and_npm/es6modules/src/config.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/src/config.js
@@ -1,22 +1,34 @@
+/*
+  Note: variables are not global with modules. They are scoped
+  to either their function, block, or the module. This is
+  a benefit of modules, self contained files that helps avoid
+  pollution of the global namespace and potential naming collisions.
+*/
+
 // Named Export
+// variables
 export const apiKey = "abc123";
 
 export const url = "http:/wesbos.com";
 
+// functions
 export function sayHi(name) {
   console.log(`Hello there ${name}`);
 }
 
+// exporting multiple variables at once
 const age = 1000;
 const cat = "Mose";
 
 export { age as old, cat };
+// `as` keyword used to rename
+
 /*
   Default Export and Named Export
 
   default export: generally used to export the main
   part of what the module does. When importing a default export
-  any variable name (apiKey) can be used.
+  any variable name can be used.
 
   named export: Named exports are useful to export several values.
   During the import, it is mandatory to use the same name of the
@@ -26,9 +38,9 @@ export { age as old, cat };
   multiple named exports.
 */
 
-/*
-  exporting as the default. Note: default exports get renamed
-  to whatever name given when imported.
-*/
+
+// exporting as the default. Note: default exports get renamed
+// to whatever name given when imported. So when importing this
+// example, `apiKey` can be changed to something else.
 
 // export default apiKey;
