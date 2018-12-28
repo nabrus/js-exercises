b4295708c29b53afbd8a9ad9593f8f4b20222d63
towelie <34988577+nabrus@users.noreply.github.com>
2018-12-28 15:14:58 -0500

    add import shuffle(lodash), with example

--- wesbos-es6/JS_modules_and_npm/es6modules/app.js
+++ wesbos-es6/JS_modules_and_npm/es6modules/app.js
@@ -1,4 +1,4 @@
-import { uniq } from "lodash";
+import { uniq, shuffle } from "lodash";
 import insane from "insane";
 import jsonp from "jsonp";
 // import apiKey from "./src/config"; // import default syntax
@@ -15,6 +15,9 @@ console.log(key, url, old, cat);
 
 sayHi("Steve");
 
-const ages = [1, 14, 56, 5, 14, 1, 1, 1];
 // using lodash to get the uniq array of ages
+const ages = [1, 14, 56, 5, 14, 1, 1, 1];
 console.log(uniq(ages));
+// lodash shuffle
+const tracks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(shuffle(tracks));
