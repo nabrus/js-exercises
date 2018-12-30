5fa7cc6d0c42322fc5844140d01078cdcaddd873
towelie <34988577+nabrus@users.noreply.github.com>
2018-12-30 17:02:38 -0500

    run script successful, add browserlist to package.json

---
+++ tooling/babel_7/compiled/main.js
@@ -0,0 +1,63 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+/* eslint no-underscore-dangle: 0 */
+var cat = "Mose";
+var numbers = [2, 5, 6, 4, 22, 32];
+var dblNums = numbers.map(function (num) {
+  return num * 2;
+});
+console.log(dblNums);
+var sum = numbers.reduce(function (acc, next) {
+  return acc + next;
+});
+console.log(sum);
+
+var Rebels =
+/*#__PURE__*/
+function () {
+  function Rebels(name) {
+    _classCallCheck(this, Rebels);
+
+    this._name = name;
+    this._hasForce = false;
+  } // Getters
+
+
+  _createClass(Rebels, [{
+    key: "greeting",
+    // Methods
+    value: function greeting() {
+      return "Hey, I'm ".concat(this.name, " and I hate the empire.");
+    }
+  }, {
+    key: "name",
+    get: function get() {
+      return this._name;
+    }
+  }, {
+    key: "force",
+    get: function get() {
+      return this._hasForce;
+    } // Setters
+    ,
+    set: function set(able) {
+      if (able === "yes") {
+        this._hasForce = true;
+      }
+    }
+  }]);
+
+  return Rebels;
+}();
+
+var luke = new Rebels("Luke Skywalker");
+var han = new Rebels("Han Solo");
+luke.force = "yes";
+console.log(luke.force);
+console.log(han.greeting());
