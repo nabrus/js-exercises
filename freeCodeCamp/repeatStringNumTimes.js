100ee8ee25ef6e158b413a6fb51f924d2a8a0227
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-15 08:38:17 -0500

    add/start repeatStringNumTimes.js exercise

---
+++ freeCodeCamp/repeatStringNumTimes.js
@@ -0,0 +1,13 @@
+/*
+  Repeat a given string str (first argument) for num times (second argument).
+  Return an empty string if num is not a positive number.
+
+  The built-in repeat()-method should not be used.
+*/
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  return str;
+}
+
+repeatStringNumTimes("abc", 3);
