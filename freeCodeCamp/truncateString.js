71d2ea3938ff29edc29f20da478fb6d4073480c7
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-17 02:04:40 -0500

    start function, short str complete

--- freeCodeCamp/truncateString.js
+++ freeCodeCamp/truncateString.js
@@ -1,6 +1,8 @@
 function truncateString(str, num) {
-  // Clear out that junk in your trunk
-  return str;
+  const shorter = str.slice(0, num);
+  console.log(`${shorter}...`);
+  return `${shorter}...`;
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+truncateString("Absolutely Longer", 2);
