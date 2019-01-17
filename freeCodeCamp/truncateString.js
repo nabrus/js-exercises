2a78c65dc6287845d3003e55f0452ccb81306225
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-17 08:51:55 -0500

    add logic, fail equal to length test

--- freeCodeCamp/truncateString.js
+++ freeCodeCamp/truncateString.js
@@ -1,8 +1,19 @@
+/*
+  Truncate a string (first argument) if it is longer than the given maximum
+  string length (second argument). Return the truncated string with a ...
+  ending.
+*/
+
+
+// My solution
 function truncateString(str, num) {
   const shorter = str.slice(0, num);
-  console.log(`${shorter}...`);
-  return `${shorter}...`;
+  console.log(str.length >= num ? `${shorter}...` : str);
+  return str.length >= num ? `${shorter}...` : str;
+
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
 truncateString("Absolutely Longer", 2);
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2);
+truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length);
