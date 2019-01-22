a9b0827a79b611dd606fd59082f6d071b962edb4
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-22 08:40:30 -0500

    add instruct, func start

---
+++ freeCodeCamp/sliceAndSplice.js
@@ -0,0 +1,20 @@
+/*
+  You are given two arrays and an index.
+
+  Use the array methods slice and splice to copy each element of the first
+  array into the second array, in order.
+
+  Begin inserting elements at index n of the second array.
+
+  Return the resulting array. The input arrays should remain the same after
+  the function runs.
+
+  frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
