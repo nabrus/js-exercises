0f1e788832bd285ff320b9e61cab2ed15683a8fc
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-22 22:31:14 -0500

    complete my solution, first take

--- freeCodeCamp/sliceAndSplice.js
+++ freeCodeCamp/sliceAndSplice.js
@@ -12,9 +12,19 @@
   frankenSplice([1, 2, 3], [4, 5], 1) should return [4, 1, 2, 3, 5].
 */
 
+// My solution: passes tests, ugly code
+// function frankenSplice(arr1, arr2, n) {
+//   const arr3 = arr1.slice();
+//   const arr4 = arr2.slice();
+//   arr4.splice(n, 0, ...arr3);
+//   return arr4;
+// }
+
+
 function frankenSplice(arr1, arr2, n) {
-  // It's alive. It's alive!
-  return arr2;
+  const newArr2 = arr2.slice();
+  arr1.forEach(num => num.splice(n, 0, newArr2));
+  console.log(newArr2);
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
