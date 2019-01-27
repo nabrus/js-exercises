a4af3b576545f58e659442938b88a0264d9b8a6e
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-27 13:20:19 -0500

    add freeCodeCamp solutions

--- freeCodeCamp/whereDoIBelong.js
+++ freeCodeCamp/whereDoIBelong.js
@@ -12,36 +12,71 @@
 */
 
 // My solutions: using a for(...) loop
-// function getIndexToIns(arr, num) {
-//   const sorted = arr.sort((a, b) => a - b);
-//   let index = 0;
-//   for (let i = 0; i < sorted.length; i += 1) {
-//     if (sorted[i] >= num) {
-//       index = i;
-//       break;
-//     } else if (sorted[i] < num) {
-//       index = i + 1;
-//     }
-//   }
-//   // return index;
-//   console.log(index);
-// }
-
-// Using for iterating 
 function getIndexToIns(arr, num) {
   const sorted = arr.sort((a, b) => a - b);
-  const answer = sorted.forEach((item, i) => {
-    
-  });
-
+  let index = 0;
+  for (let i = 0; i < sorted.length; i += 1) {
+    if (sorted[i] >= num) {
+      index = i;
+      break;
+    } else if (sorted[i] < num) {
+      index = i + 1;
+    }
+  }
+  // return index;
+  console.log(index);
 }
 
-// getIndexToIns([40, 60], 50); // 1
-// getIndexToIns([40, 4, 17, 6], 14); // 2
-// getIndexToIns([20, 3, 5], 14); // 2
-// getIndexToIns([10, 20, 30, 40, 50], 35); // 3
-// getIndexToIns([2, 5, 10], 15); // 3
-// getIndexToIns([], 1); // 0
 
-const test = [10, 20, 30, 40, 50].forEach((num, i) => console.log(num > 40, i));
-test;
+// freeCodeCamp solutions:
+
+// Basic
+function getIndexToIns1(arr, num) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var a = 0; a < arr.length; a++) {
+    if (arr[a] >= num)
+      return a;
+  }
+  return arr.length;
+}
+
+// Basic: initial direction I tried to take for solving.
+function getIndexToIns2(arr, num) {
+
+  arr.sort(function(a, b) {
+  return a - b;
+  });
+
+  var i = 0;
+  while (num > arr[i]) {
+  i++;
+  }
+
+  return i;
+}
+
+// Intermediate: clever solution, adding the number `num` to the array
+// then find the index of it.
+function getIndexToIns3(arr, num) {
+  arr.push(num);
+  arr.sort(function(a, b){return a-b});
+  return arr.indexOf(num);
+}
+
+// Advanced: Same idea as last example
+function getIndexToIns4(arr, num) {
+
+  return arr.concat(num).sort((a,b) => a-b).indexOf(num);
+  
+  }
+
+
+getIndexToIns([40, 60], 50); // 1
+getIndexToIns([40, 4, 17, 6], 14); // 2
+getIndexToIns([20, 3, 5], 14); // 2
+getIndexToIns([10, 20, 30, 40, 50], 35); // 3
+getIndexToIns([2, 5, 10], 15); // 3
+getIndexToIns([], 1); // 0
