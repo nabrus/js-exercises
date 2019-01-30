fa756195b35597679d2c65ee1d6107f4bfa571c2
towelie <34988577+nabrus@users.noreply.github.com>
2019-01-29 21:24:25 -0500

    add chunky monkey file w/ instructions

---
+++ freeCodeCamp/chunkyMonkey.js
@@ -0,0 +1,11 @@
+/*
+  Write a function that splits an array (first argument) into groups the
+  length of size (second argument) and returns them as a two-dimensional
+  array.
+
+  chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"],
+  ["c","d"]].
+
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2],
+  [3, 4, 5]].
+*/
