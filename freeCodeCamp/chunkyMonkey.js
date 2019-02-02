e37e31fc121cea7fbb00f25bcd9f0aafb802c3db
towelie <34988577+nabrus@users.noreply.github.com>
2019-02-01 23:22:26 -0500

    add freeCodeCamp solutions

--- freeCodeCamp/chunkyMonkey.js
+++ freeCodeCamp/chunkyMonkey.js
@@ -22,20 +22,49 @@
 
 // refactor to eliminate .splice(..), it mutates orig array
 // This is the advanced freeCodeCamp solution
-// function chunkArrayInGroups(arr, size) {
-//   const newArr = [];
-//   let i = 0;
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  let i = 0;
 
-//   while (i < arr.length) {
-//     newArr.push(arr.slice(i, i + size));
-//     i += size;
+  while (i < arr.length) {
+    newArr.push(arr.slice(i, i + size));
+    i += size;
+  }
+  console.log(newArr);
+}
+
+
+// freeCodeCamps solutions
+// basic
+// function chunkArrayInGroups(arr, size) {
+
+//   var temp = [];
+//   var result = [];
+
+//   for (var a = 0; a < arr.length; a++) {
+//     if (a % size !== size - 1)
+//       temp.push(arr[a]);
+//     else {
+//       temp.push(arr[a]);
+//       result.push(temp);
+//       temp = [];
+//     }
 //   }
-//   console.log(newArr);
+
+//   if (temp.length !== 0)
+//     result.push(temp);
+//   return result;
 // }
 
-function chunkArrayInGroups(arr, size) {
-  
-}
+// Intermediate
+// function chunkArrayInGroups(arr, size) {
+//   // Break it up.
+//   var arr2 = [];
+//   for (var i = 0; i < arr.length; i+=size) {
+//   arr2.push(arr.slice(i , i+size));
+//   }
+//   return arr2;
+// }
 
 
 chunkArrayInGroups(["a", "b", "c", "d"], 2);
