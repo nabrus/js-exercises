22aad9b72a2780e1fabbd7c97fa8ee56f0696a1d
towelie <34988577+nabrus@users.noreply.github.com>
2019-03-25 01:12:24 -0400

    add arrayReverse.js

---
+++ algorithms/arrayReverse.js
@@ -0,0 +1,14 @@
+// Cannot use the .reverse() method
+// Write your code here: 
+function reverseArray(arr) {
+  arr.forEach(word => {
+    
+  })
+}
+
+
+/* Test
+const sentence = ['sense.','make', 'all', 'will', 'This'];
+*/
+console.log(reverseArray(sentence)) 
+// Should print ['This', 'will', 'all', 'make', 'sense.'];
