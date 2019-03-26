dd82305a9f16fe89bcfc6b11feef4554010041c9
towelie <34988577+nabrus@users.noreply.github.com>
2019-03-25 22:52:32 -0400

    complete 2 solutions

--- algorithms/challenges/arrayReverse.js
+++ algorithms/challenges/arrayReverse.js
@@ -1,14 +1,29 @@
 // Cannot use the .reverse() method
-// Write your code here: 
+// Write your code here:
+
+// My solution: returns a new array
 function reverseArray(arr) {
-  arr.forEach(word => {
-    
-  })
+  const result = [];
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    result.push(arr[i]);
+  }
+  return result;
+}
+
+// An "in line" reversal of elements. Mutating the original array.
+function mutateArray(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    arr.splice(i, 0, arr.pop());
+  }
+  return arr;
 }
 
 
-/* Test
-const sentence = ['sense.','make', 'all', 'will', 'This'];
-*/
-console.log(reverseArray(sentence)) 
+// Test
+const sentence = ["sense.", "make", "all", "will", "This"];
+
+console.log(sentence);
+console.log(mutateArray(sentence));
+console.log(sentence);
+// console.log(reverseArray(sentence));
 // Should print ['This', 'will', 'all', 'make', 'sense.'];
