927580a0dc48b300857b302d3d6c463b8937a891
cartman <34988577+nabrus@users.noreply.github.com>
2019-04-22 00:02:22 -0400

    add chain adding fn challenge

---
+++ algorithms/challenges/chainAdding.js
@@ -0,0 +1,22 @@
+// A Chain adding function
+
+/* 
+  A function that adds numbers (params) together.
+  add(1)(2); // returns 3
+
+  Can take multible args chained together when called.
+  add(1)(2)(3); // 6
+  add(1)(2)(3)(4) // 10
+
+  A single call returns the number passed in.
+  add(1); // 1  
+
+  The returned value can be stored in a var and be reused
+  const addTwo = add(2);
+  addTwo; // 2
+  addTwo + 5; // 7
+  addTwo(3); // 5
+  
+  Input/Output: valid whold numbers
+
+  */
