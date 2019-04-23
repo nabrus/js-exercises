e1af63fcc64f8b7f9c9eac438a1cf46b6fbbf9a3
cartman <34988577+nabrus@users.noreply.github.com>
2019-04-23 01:11:55 -0400

    fn returns single string

--- algorithms/challenges/greetAliens.js
+++ algorithms/challenges/greetAliens.js
@@ -5,3 +5,17 @@
   Output: "Oh powerful [stringElement], we humans offer our unconditional 
   surrender!" for each element of the array.
 */ 
+
+function greetAliens(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    newArr.push(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!
+    `);
+  }
+  return newArr.join("");
+}
+
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
