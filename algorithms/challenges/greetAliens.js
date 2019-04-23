79415b6d3a5e086ee42d810e0b758f5317e6f966
cartman <34988577+nabrus@users.noreply.github.com>
2019-04-23 00:26:48 -0400

    add greetAliens.js file w/ instructions

---
+++ algorithms/challenges/greetAliens.js
@@ -0,0 +1,7 @@
+/* 
+  Function `greetAliens()` that takes in an array of strings and uses
+  a `for` loop to print a greeting with each string in the array.
+  
+  Output: "Oh powerful [stringElement], we humans offer our unconditional 
+  surrender!" for each element of the array.
+*/ 
