2304785dad4dea9f675335e8fce2d24381588ed8
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-26 13:14:52 -0400

    complete codecademy greetAliens challenge

--- algorithms/challenges/greetAliens.js
+++ algorithms/challenges/greetAliens.js
@@ -1,20 +1,16 @@
-/* 
+/*
   Function `greetAliens()` that takes in an array of strings and uses
   a `for` loop to print a greeting with each string in the array.
-  
-  Output: "Oh powerful [stringElement], we humans offer our unconditional 
-  surrender!" for each element of the array.
-*/ 
+
+  Output: "Oh powerful [stringElement], we humans offer our unconditional
+  surrender!" for each element of the array. To the console.
+*/
 
 function greetAliens(arr) {
-  let newArr = [];
   for (let i = 0; i < arr.length; i += 1) {
-    newArr.push(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!
-    `);
+    console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`);
   }
-  return newArr.join("");
 }
-
 
 const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
 
