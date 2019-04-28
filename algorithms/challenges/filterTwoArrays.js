4c297404749892c4175767c8da1e97bbe045e928
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-27 22:55:15 -0400

    add filterTwoArrays.js file

---
+++ algorithms/challenges/filterTwoArrays.js
@@ -0,0 +1,18 @@
+/*
+  Function `justCoolStuff()`
+
+  Input: Two arrays of strings
+  Output: An array with items present in both arrays.
+
+  Must use `Array.prototype.filter()` method.
+*/
+
+justCoolStuff()
+
+/* eslint quotes: [2, "single"] */
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = ['rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway'];
+
+console.log(justCoolStuff(myStuff, coolStuff));
