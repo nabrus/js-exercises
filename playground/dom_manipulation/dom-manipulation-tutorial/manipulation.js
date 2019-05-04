f1c474447dd356b569745ff88c26a00f9f30f024
towelie <34988577+nabrus@users.noreply.github.com>
2019-05-04 15:34:10 -0400

    start multiple selectors

--- playground/dom_manipulation/dom-manipulation-tutorial/manipulation.js
+++ playground/dom_manipulation/dom-manipulation-tutorial/manipulation.js
@@ -1,5 +1,29 @@
+// Accessing single elements
 const id = document.getElementById("head");
 const q = document.querySelector("div p");
 
+// Changing CSS properties of selected.
 id.style.color = "red";
 q.style.fontSize = "30px";
+
+
+// Accessing multiple elements
+const className = document.getElementsByClassName("text");
+console.log(className); // Outputs the node list of <p> with class "text"
+/*
+  HTMLCollection(3) [p#middle.text, p.text, p.text, middle: p#middle.text]
+    0: p#middle.text
+    1: p.text
+    2: p.text
+    length: 3
+    middle: p#middle.text
+   __proto__: HTMLCollection
+*/
+
+console.log(className[2]); 
+// Outputs the third element in node list:  <p class="text">Out of dev</p>
+
+console.log(className.item(2));
+// Outputs the third element in node list using the
+//`Node​List​.item()`:
+// <p class="text">Out of dev</p>
