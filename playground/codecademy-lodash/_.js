2a7ffa4ae2a5757696e4fee44b5ccdc1b539130a
towelie <34988577+nabrus@users.noreply.github.com>
2019-05-22 09:15:51 -0400

    add project description, complete step one

--- playground/codecademy-lodash/_.js
+++ playground/codecademy-lodash/_.js
@@ -1,6 +1,11 @@
+/*
+  In this Lodash project, you will be implementing some of the most exciting
+  functionality from the widely-popular lodash.js library. You will be
+  implementing ten methods that add new functionality for numbers, strings,
+  objects, and arrays. -codecademy
+*/
 
-
-
+const _ = {};
 
 
 // Do not write or modify code below this line.
