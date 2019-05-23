ecde0d9fd8ba21466cb957f67386130362297dc7
towelie <34988577+nabrus@users.noreply.github.com>
2019-05-22 23:49:19 -0400

    complete clamp method

--- playground/codecademy-lodash/_.js
+++ playground/codecademy-lodash/_.js
@@ -5,7 +5,13 @@
   objects, and arrays. -codecademy
 */
 
-const _ = {};
+const _ = {
+  clamp(number, lower, upper) {
+    const lowerClampedValue = Math.max(number, lower);
+    const clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
+};
 
 
 // Do not write or modify code below this line.
