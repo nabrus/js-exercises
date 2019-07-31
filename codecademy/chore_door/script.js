3593254b550901a45937de71894ec346479d36b4
towelie <34988577+nabrus@users.noreply.github.com>
2019-07-31 18:14:21 -0400

    complete three doors w/images, onclick events

---
+++ codecademy/chore_door/script.js
@@ -0,0 +1,22 @@
+// Closed Doors 1-3
+const doorImage1 = document.querySelector("#door1");
+const doorImage2 = document.querySelector("#door2");
+const doorImage3 = document.querySelector("#door3");
+
+// Open Door Images
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+
+// Doors 1-3 Click Event Functions
+doorImage1.onclick = () => {
+  doorImage1.src = botDoorPath;
+};
+
+doorImage2.onclick = () => {
+  doorImage2.src = beachDoorPath;
+};
+
+doorImage3.onclick = () => {
+  doorImage3.src = spaceDoorPath;
+};
