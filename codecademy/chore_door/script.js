2ef5e38c6814acb276d38706861677d7f92a4d14
cartman <34988577+nabrus@users.noreply.github.com>
2019-08-10 22:51:43 -0400

    complete randomBotGen function

--- codecademy/chore_door/script.js
+++ codecademy/chore_door/script.js
@@ -9,20 +9,39 @@ const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chor
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 
 const numClosedDoors = 3;
+let openDoor1 = " ";
+let openDoor2 = " ";
+let openDoor3 = " ";
 
 const randomChoreDoorGenerator = () => {
   const choreDoor = Math.floor(Math.random() * numClosedDoors);
+
+  if (choreDoor === 0) {
+    openDoor1 = botDoorPath;
+    openDoor2 = beachDoorPath;
+    openDoor3 = spaceDoorPath;
+  } else if (choreDoor === 1) {
+    openDoor1 = spaceDoorPath;
+    openDoor2 = botDoorPath;
+    openDoor3 = beachDoorPath;
+  } else {
+    openDoor1 = beachDoorPath;
+    openDoor2 = spaceDoorPath;
+    openDoor3 = botDoorPath;
+  }
 };
 
 // Doors 1-3 Click Event Functions
 doorImage1.onclick = () => {
-  doorImage1.src = botDoorPath;
+  doorImage1.src = openDoor1;
 };
 
 doorImage2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+  doorImage2.src = openDoor2;
 };
 
 doorImage3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
+  doorImage3.src = openDoor3;
 };
+
+randomChoreDoorGenerator();
