ffab0533507e82d3d83f7890f48086a6cbf49c5b
towelie <34988577+nabrus@users.noreply.github.com>
2019-09-02 13:29:40 -0400

    complete home page template

--- codecademy/musicon/public/main.js
+++ codecademy/musicon/public/main.js
@@ -34,3 +34,6 @@ const context = {
 const templateElement = document.getElementById("templateHB");
 const templateSource = templateElement.innerHTML;
 const template = Handlebars.compile(templateSource);
+const compiledHtml = template(context);
+
+document.getElementById("information").innerHTML = compiledHtml;
