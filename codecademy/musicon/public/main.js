e0e7635c5f46cfd37b69506c8941ad2bc20ba5c9
towelie <34988577+nabrus@users.noreply.github.com>
2019-09-02 13:25:26 -0400

    add three vars after context obj on main.js

--- codecademy/musicon/public/main.js
+++ codecademy/musicon/public/main.js
@@ -7,26 +7,30 @@ const context = {
       name: "Electronic Keyboard",
       description: "A piano welcomed to the 21th century. Pianists celebrate the compact form factor and the diversity of synthesized rhythms all in one masterful musical machine.",
       price: "$1,999.00",
-      sale: "$1,699.89"
+      sale: "$1,699.89",
     },
     {
       image: "https://s3.amazonaws.com/codecademy-content/courses/learn-handlebars/musicon/electric-guitar.png",
       name: "Electric Guitar",
       description: "Join the legends of the '50s and '60s when the marriage of guitar and electricity created the most influential instrument of a generation. Note: picks sold separately.",
-      price: "$599.99"
+      price: "$599.99",
     },
     {
       image: "https://s3.amazonaws.com/codecademy-content/courses/learn-handlebars/musicon/bass-guitar.png",
       name: "Bass Guitar",
       description: "Experience the embodiment of funkadelic frequencies that is the bass guitar. Let the deep low notes of the bass guitar resonate with heartbeats everywhere.",
-      price: "$624.99"
+      price: "$624.99",
     },
     {
       image: "https://s3.amazonaws.com/codecademy-content/courses/learn-handlebars/musicon/drum-kit.png",
       name: "Drum Kit",
       description: "Ever thought, \"one instrument isn't enough?\" Find an answer in the drum kit. Coordinate a collections of drums and cymbals to dictate the rhythm of musical masterpiece.",
       price: "$649.00",
-      sale: "$349.00"
-    }
-  ]
+      sale: "$349.00",
+    },
+  ],
 };
+
+const templateElement = document.getElementById("templateHB");
+const templateSource = templateElement.innerHTML;
+const template = Handlebars.compile(templateSource);
