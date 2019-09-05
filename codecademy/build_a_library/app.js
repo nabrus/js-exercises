1be0739af75b4758c2c49837f20a2a7f6ec5b0a3
towelie <34988577+nabrus@users.noreply.github.com>
2019-09-05 07:37:43 -0400

    add getters for props

--- codecademy/build_a_library/app.js
+++ codecademy/build_a_library/app.js
@@ -1,7 +1,20 @@
 class Media {
   constructor(title) {
-    _this.title = title;
-    _this.isCheckedOut = false;
-    _this.ratings = [];
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  // Getters
+  get title() {
+    return this._title;
+  }
+
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+
+  get ratings() {
+    return this._ratings;
   }
 }
