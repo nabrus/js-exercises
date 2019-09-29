d905dc1cf91713bb5e7fc85e324bae705d9e8cea
towelie <34988577+nabrus@users.noreply.github.com>
2019-09-29 07:21:33 -0400

    move displayfn to message.js

--- codecademy/message_mixer/messageMixer.js
+++ codecademy/message_mixer/messageMixer.js
@@ -51,13 +51,6 @@ function encode(string) {
 }
 
 
-function displayMessage() {
-  console.log(countCharacter("What is the color of the sky?", "t"));
-  console.log(capitalizeFirstCharacterOfWords("What is the color of the sky?"));
-  console.log(reverseWord("What is the color of the sky?"));
-  console.log(reverseAllWords("What is the color of the sky?"));
-  console.log(replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
-  console.log(encode("What is the color of the sky?"));
-}
+
 
 displayMessage();
