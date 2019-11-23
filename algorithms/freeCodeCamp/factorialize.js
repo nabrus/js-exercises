4a1142df0ddbbcb31ecd165cb9d31aee01f3e4a1
towelie <34988577+nabrus@users.noreply.github.com>
2019-11-23 10:08:22 -0500

    add factorialize a number starter code

---
+++ algorithms/freeCodeCamp/factorialize.js
@@ -0,0 +1,23 @@
+/*
+Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of
+all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+*/
+
+function factorialize(num) {
+  return num;
+}
+
+console.log(factorialize(5));
+console.log(factorialize(10));
+console.log(factorialize(20));
+console.log(factorialize(0));
