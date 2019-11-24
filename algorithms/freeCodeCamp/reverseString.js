a6e7a76d4de0a9fd7af7f7dad09cbb1e49d2c6a0
towelie <34988577+nabrus@users.noreply.github.com>
2019-11-24 10:08:31 -0500

    add non reverse() example

--- algorithms/freeCodeCamp/reverseString.js
+++ algorithms/freeCodeCamp/reverseString.js
@@ -15,6 +15,17 @@ Or use the spread operator [...]
 `return [...str].reverse().join("");`
 */
 
+function reverseStringA(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
 console.log(reverseString("hello"));
+console.log(reverseStringA("hello"));
 console.log(reverseString("Howdy"));
+console.log(reverseStringA("Howdy"));
 console.log(reverseString("Greetings from Earth"));
+console.log(reverseStringA("Greetings from Earth"));
