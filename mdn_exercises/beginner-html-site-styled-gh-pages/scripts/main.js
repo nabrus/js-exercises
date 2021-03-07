a851a423707dc4f8e67086bc30f7ae85abf28b28
butters <34988577+nabrus@users.noreply.github.com>
2021-03-07 18:13:35 -0500

    Add setUserName function

--- mdn_exercises/beginner-html-site-styled-gh-pages/scripts/main.js
+++ mdn_exercises/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -1,5 +1,8 @@
 let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
 
+// Cycle between pics 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/firefox-icon.png') {
@@ -7,4 +10,11 @@ myImage.onclick = function() {
   } else {
     myImage.setAttribute('src', 'images/firefox-icon.png');
   }
+}
+
+// Personalize greeting
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = `Mozilla is cool, ${myName}`;
 }
