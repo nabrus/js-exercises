5557b084e1bf3da9b15bf1a302720487caf1cee5
butters <34988577+nabrus@users.noreply.github.com>
2021-03-08 20:12:41 -0500

    Add condition to correct setUserName func when no value is entered

--- mdn_exercises/beginner-html-site-styled-gh-pages/scripts/main.js
+++ mdn_exercises/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -15,8 +15,12 @@ myImage.onclick = function() {
 // Personalize greeting
 function setUserName() {
   let myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = `Mozilla is cool, ${myName}, but so is Apple.`;
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}, but so is Apple.`;
+  }
 }
 
 // Initialization at load
