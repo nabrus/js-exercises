18fb0fc79f5782ec204457875adf04f35a8ab0c6
butters <34988577+nabrus@users.noreply.github.com>
2021-11-13 13:58:10 -0500

    Add js and css to html doc

---
+++ playground/basic-tetris/app.js
@@ -0,0 +1,6 @@
+// The DOMContentLoaded event fires after  HTML doc is loaded on page. 
+// An alterative to putting script tag right before the end of the doc
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("hello");
+
+})
