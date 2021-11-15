3fb7aa3cf37d5efdcc3afbb6553c12ff73a17fe1
butters <34988577+nabrus@users.noreply.github.com>
2021-11-14 19:06:42 -0500

    Add starter vars

--- playground/basic-tetris/app.js
+++ playground/basic-tetris/app.js
@@ -1,6 +1,9 @@
 // The DOMContentLoaded event fires after  HTML doc is loaded on page. 
 // An alterative to putting script tag right before the end of the doc
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("hello");
+
+  const grid = document.querySelector('.grid');
+  let squares = document.querySelectorAll('.grid div');
+  const width = 10
 
 })
