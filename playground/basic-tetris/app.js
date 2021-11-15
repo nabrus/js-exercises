9494cdd2ecc0c886391f06fcd1d05c23e198b4d6
butters <34988577+nabrus@users.noreply.github.com>
2021-11-14 19:19:33 -0500

    Complete intial vars

--- playground/basic-tetris/app.js
+++ playground/basic-tetris/app.js
@@ -3,7 +3,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const grid = document.querySelector('.grid');
-  let squares = document.querySelectorAll('.grid div');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  // Array.from changes squares from a node list to an array
+  const scoreDisplay = document. querySelector('#score');
+  const startBtn = document. querySelector('#start-button');
   const width = 10
+
+
 
 })
