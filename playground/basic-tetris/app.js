eb3115b4374402acc41de95929a9e7321e13eeea
butters <34988577+nabrus@users.noreply.github.com>
2021-11-22 19:05:14 -0500

    Create tetrominoes

--- playground/basic-tetris/app.js
+++ playground/basic-tetris/app.js
@@ -9,6 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const startBtn = document. querySelector('#start-button');
   const width = 10
 
+  // The Tetrominoes
+  const lTetrominoe = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
 
-
+  console.log(lTetrominoe);
 })
