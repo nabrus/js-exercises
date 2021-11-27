e354778f686fb9f220181866a5813bf630550c3d
butters <34988577+nabrus@users.noreply.github.com>
2021-11-27 12:49:25 -0500

    Add style for tetromino, start drawing tetromino

--- playground/basic-tetris/app.js
+++ playground/basic-tetris/app.js
@@ -10,42 +10,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
 
   // The Tetrominoes
-  const lTetrominoe = [
+  const lTetromino = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2],
     [width, width*2, width*2+1, width*2+2]
   ]
 
-  const zTetrominoe = [
+  const zTetromino = [
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1],
     [0, width, width+1, width*2+1],
     [width+1, width+2, width*2, width*2+1],
   ]
 
-  const tTetrominoe = [
+  const tTetromino = [
     [1, width, width+1, width+2],
     [1, width+1, width+2, width*2+1],
     [width, width+1, width+2, width*2+1],
     [1, width, width+1, width*2+1],
   ]
 
-  const oTetrominoe = [
+  const oTetromino = [
     [0, 1, width, width+1],
     [0, 1, width, width+1],
     [0, 1, width, width+1],
     [0, 1, width, width+1],
   ]
 
-  const iTetrominoe = [
+  const iTetromino = [
     [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3],
     [1, width+1, width*2+1, width*3+1],
     [width, width+1, width+2, width+3],
   ]
 
-  const theTetrominoes = [lTetrominoe, zTetrominoe, tTetrominoe, oTetrominoe, iTetrominoe]
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
-  console.log(theTetrominoes);
+  let currentPosition  = 4;
+  let current = theTetrominoes[0][0];
+
+  // Draw the first rotation in the first tetromino
+  
 })
