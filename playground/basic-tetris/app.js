e97fd2046db6a7a3d36ea9c3a3d554c1262d055f
towelie <34988577+nabrus@users.noreply.github.com>
2022-01-02 11:48:17 -0500

    Update to format, spacing etc

--- playground/basic-tetris/app.js
+++ playground/basic-tetris/app.js
@@ -1,63 +1,63 @@
-// The DOMContentLoaded event fires after  HTML doc is loaded on page. 
+// The DOMContentLoaded event fires after  HTML doc is loaded on page.
 // An alterative to putting script tag right before the end of the doc
-document.addEventListener('DOMContentLoaded', () => {
-
-  const grid = document.querySelector('.grid');
-  let squares = Array.from(document.querySelectorAll('.grid div'));
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const squares = Array.from(document.querySelectorAll(".grid div"));
   // Array.from changes squares from a node list to an array
-  const scoreDisplay = document. querySelector('#score');
-  const startBtn = document. querySelector('#start-button');
-  const width = 10
+  const scoreDisplay = document.querySelector("#score");
+  const startBtn = document.querySelector("#start-button");
+  const width = 10;
 
   // The Tetrominoes
   const lTetromino = [
-    [1, width+1, width*2+1, 2],
-    [width, width+1, width+2, width*2+2],
-    [1, width+1, width*2+1, width*2],
-    [width, width*2, width*2+1, width*2+2]
-  ]
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
 
   const zTetromino = [
-    [0, width, width+1, width*2+1],
-    [width+1, width+2, width*2, width*2+1],
-    [0, width, width+1, width*2+1],
-    [width+1, width+2, width*2, width*2+1],
-  ]
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
 
   const tTetromino = [
-    [1, width, width+1, width+2],
-    [1, width+1, width+2, width*2+1],
-    [width, width+1, width+2, width*2+1],
-    [1, width, width+1, width*2+1],
-  ]
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
 
   const oTetromino = [
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-    [0, 1, width, width+1],
-  ]
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
 
   const iTetromino = [
-    [1, width+1, width*2+1, width*3+1],
-    [width, width+1, width+2, width+3],
-    [1, width+1, width*2+1, width*3+1],
-    [width, width+1, width+2, width+3],
-  ]
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
 
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
 
-  let currentPosition  = 4;
+  const currentPosition = 4;
+  const currentRotation = 0;
   // Randomly select a Tetromino and its first rotation
-  let random = Math.floor(Math.random()*theTetrominoes.length);
-  let current = theTetrominoes[random][0];
+  const random = Math.floor(Math.random() * theTetrominoes.length);
+  const current = theTetrominoes[random][currentRotation];
 
   // Draw the first rotation in the first Tetromino
   function draw() {
     current.forEach(index => {
-      squares[currentPosition + index].classList.add('tetromino');
-    })
+      squares[currentPosition + index].classList.add("tetromino");
+    });
   }
-  
+
   draw();
-})
+});
