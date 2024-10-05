59a8fb2254a74aeabe72628e5789eee6f9f1950a
butters <34988577+nabrus@users.noreply.github.com>
2024-10-04 23:25:43 -0400

    Complete 3 map challenges

--- looping/mapFilterReduce.js
+++ looping/mapFilterReduce.js
@@ -1,5 +1,3 @@
-
-
 /*
 1. Challenges for map():
 
@@ -9,6 +7,10 @@ Write a function that takes an array of numbers and returns a new array where ea
 const numbers = [1, 2, 3, 4];
 // Expected output: [1, 4, 9, 16]
 
+const squareNums = numbers.map(num => num ** 2);
+console.log(squareNums); // [1, 4, 9, 16]
+
+console.log('----------------------------------');
 /*
 Extract property from an array of objects:
 Given an array of user objects, use map() to create an array of just the user names.
@@ -20,13 +22,30 @@ const users = [
 ];
 // Expected output: ['Alice', 'Bob', 'Charlie']
 
+function onlyUsers(array) {
+  return array.name;
+}
+
+const userNames = users.map(onlyUsers);
+console.log(userNames); // ['Alice', 'Bob', 'Charlie']
+
+const userNames1 = users.map(names => names.name);
+console.log(userNames1); // ['Alice', 'Bob', 'Charlie']
+
+console.log('----------------------------------');
 /*
 Convert Fahrenheit to Celsius:
-Write a function that takes an array of temperatures in Fahrenheit and converts them to Celsius.
+Write a function that takes an array of temperatures in Fahrenheit and converts them to Celsius.  °C = (°F - 32) × 5/9
 */
 const fahrenheitTemps = [32, 68, 100];
 // Expected output: [0, 20, 37.78]
 
+function toCelsius(temps) {
+  return (temps - 32) * 5/9;
+}
+
+const fahrenheitToCelsius = fahrenheitTemps.map(toCelsius);
+console.log(fahrenheitToCelsius); // [0, 20, 37.77777777777778]
 
 /*
 2. Challenges for filter():
@@ -34,14 +53,14 @@ const fahrenheitTemps = [32, 68, 100];
 Filter even numbers from an array:
 Write a function that takes an array of numbers and returns only the even numbers.
 */
-const numbers = [1, 2, 3, 4, 5, 6];
+const filterNumbers = [1, 2, 3, 4, 5, 6];
 // Expected output: [2, 4, 6]
 
 /*
 Find people over a certain age:
 Given an array of user objects, use filter() to return users who are 30 or older.
 */
-const users = [
+const filterUsers = [
   { name: 'Alice', age: 25 },
   { name: 'Bob', age: 30 },
   { name: 'Charlie', age: 35 }
@@ -52,7 +71,7 @@ const users = [
 Filter out words shorter than 5 letters:
 Given an array of words, use filter() to return only the words that have 5 or more letters.
 */
-const words = ['apple', 'hi', 'banana', 'cat', 'elephant'];
+const filterWords = ['apple', 'hi', 'banana', 'cat', 'elephant'];
 // Expected output: ['apple', 'banana', 'elephant']
 
 
@@ -61,7 +80,7 @@ const words = ['apple', 'hi', 'banana', 'cat', 'elephant'];
 Sum all numbers in an array:
 Write a function that uses reduce() to return the sum of all numbers in an array.
 */
-const numbers = [1, 2, 3, 4];
+const reduceNumbers = [1, 2, 3, 4];
 // Expected output: 10
 
 /*
